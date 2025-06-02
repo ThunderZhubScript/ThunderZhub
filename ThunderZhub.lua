fa376ce36583c7355e37a123c11a35f45451b366
ThunderZhubScript <eubdbusbte@gmail.com>
2025-06-02 23:18:58 +0700

    Update ThunderZhub.lua

--- ThunderZhub.lua
+++ ThunderZhub.lua
@@ -1,8 +1,6 @@
 -- สร้างตารางเก็บ PlaceId และลิงก์ของสคริปต์
 local allowedMaps = {
     [16732694052] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/main/ThunderZhub-Fisch.lua",  -- Fisch
-    [87854376962069] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/main/ThunderZhub-The1.000.000GlassBridge.lua",  -- The $1,000,000 Glass Bridge
-    [16993432698] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/main/ThunderZhub-Impossible-Squid-Game-Glass-Bridge-2.lua",  -- Impossible Squid Game! Glass Bridge 2
     [13827198708] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/main/ThunderZhub-Pull-a-Sword.lua",  -- Pull a Sword
     [17534163435] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/refs/heads/main/Dungeon-RNG.lua" -- Dungeon RNG
 }
