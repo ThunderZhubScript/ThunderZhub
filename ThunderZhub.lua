7c5aaebf2f7d5836ce2c293bf966a16aa7a4366c
ThunderZhubScript <eubdbusbte@gmail.com>
2025-07-25 15:09:53 +0700

    Update ThunderZhub.lua

--- ThunderZhub.lua
+++ ThunderZhub.lua
@@ -2,7 +2,7 @@
 local allowedMaps = {
     [16732694052] = "https://github.com/ThunderZhubScript/ThunderZ-hub/blob/main/ThunderZhub-Fisch.lua",  -- Fisch
     [13827198708] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/main/ThunderZhub-Pull-a-Sword.lua",  -- Pull a Sword
-    [17534163435] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/refs/heads/main/Dungeon-RNG.lua", -- Dungeon RNG
+    [17534163435] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/refs/heads/main/ThunderZhub-Dungeon-RNG.lua", -- Dungeon RNG
     [110829983956014] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/refs/heads/main/ThunderZhub-Anime-Card-Clash.lua", -- Anime Card Clash
     [4520749081] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/refs/heads/main/ThunderZhub-King-Legacy.lua" -- King legacy
 }
