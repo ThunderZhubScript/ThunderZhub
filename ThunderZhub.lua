6618819524288ba2fd49ab39f1574d75075c7311
ThunderZhubScript <eubdbusbte@gmail.com>
2025-07-25 15:08:47 +0700

    Update ThunderZhub.lua

--- ThunderZhub.lua
+++ ThunderZhub.lua
@@ -1,6 +1,6 @@
 -- สร้างตารางเก็บ PlaceId และลิงก์ของสคริปต์
 local allowedMaps = {
-    [16732694052] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/main/ThunderZhub-Fisch.lua",  -- Fisch
+    [16732694052] = "https://github.com/ThunderZhubScript/ThunderZ-hub/blob/main/ThunderZhub-Fisch.lua",  -- Fisch
     [13827198708] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/main/ThunderZhub-Pull-a-Sword.lua",  -- Pull a Sword
     [17534163435] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/refs/heads/main/Dungeon-RNG.lua", -- Dungeon RNG
     [110829983956014] = "https://raw.githubusercontent.com/ThunderZhubScript/ThunderZ-hub/refs/heads/main/ThunderZhub-Anime-Card-Clash.lua", -- Anime Card Clash
